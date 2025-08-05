31d18bfcd87a21f5606c0d6201c34f06c5e3a44a
Berangere Br <brunberangere@gmail.com>
2025-08-05 16:47:52 +0200

    collapse/404

--- my-react-app/src/components/Collapse.jsx
+++ my-react-app/src/components/Collapse.jsx
@@ -1,17 +1,32 @@
 import { useState } from "react"
 import datas from '../../about.json'
+import closeVecteur from '../assets/closeVecteur.png'
+import openVecteur from '../assets/openVecteur.png'
+
 
 function Collapse() {
-  const [openIndex, setOpenIndex] = useState(false);
-
+  const [open, setOpen] = useState([]);
+  const isOpen = (index) => { 
+    if (open.includes(index)){
+      setOpen(open.filter((i) => i !==index));
+    }else{
+      setOpen([...open, index])
+    }
+  }
+  
   return (
-    <div>
+    <div className="containerCollapse">
       {datas.map((data, index) => (
-        <div key={index}>
-          <button onClick={() => setOpenIndex(openIndex === index ? null : index)}>
-            {data.titre}
-          </button>
-          {openIndex === index && <p>{data.paragraphe}</p>}
+        <div className="collapse" key={index}>
+          <div>
+            <div className="collapse__title">
+              <h1>{data.titre}</h1>
+              <button onClick={() => isOpen(index)}>
+                <img src={open.includes(index) ? openVecteur : closeVecteur} alt="vecteur"/>
+              </button>
+            </div>
+          </div>
+          {open.includes(index) && <p>{data.paragraphe}</p>}
         </div>
       ))}
     </div>
--- my-react-app/src/pages/Error.jsx
+++ my-react-app/src/pages/Error.jsx
@@ -9,8 +9,8 @@ function Error () {
                 <div className='error'> 
                     <p className='number'>404</p>
                     <p>Oups! La page que vous demandez n'existe pas.</p>
+                    <Link to="/">Retourner sur la page d'accueil.</Link>
                 </div>
-                <Link to="/">Retourner sur la page d'accueil.</Link>
             </div>
         </div>
     )
